f256696f1da33ce1cdc13e0ff4408a103cbcd6f4
OtraPersona <jonatan.a.ballestero@gmail.com>
2024-05-13 16:27:31 -0300

    Paleta de colores actualizada. Carrousel casi terminado

--- JS/banner.js
+++ JS/banner.js
@@ -3,19 +3,22 @@ let indice = 0;
 function mostrarImg() {
   const img = document.getElementsByClassName("img");
   
+  //Iterar sobre las imagenes cargadas para que no se vean
   for (let i = 0; i < img.length; i++) {
     img[i].style.display = "none";
   }
-  
   indice++;
   
-  if (indice > img.length) {indice = 1}
+  if (indice >= img.length) {
+    indice = 0;
+  } else if (indice <= 0) {
+    indice = img.length - 1;
+  }
   
-  img[indice - 1].style.display = "block";
+  img[indice].style.display = "block";
 }
 
 document.getElementById("prevBtn").addEventListener("click", () => {
-  indice--;
   mostrarImg();
 });
 
