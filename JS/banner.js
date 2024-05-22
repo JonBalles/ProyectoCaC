e6ffc59c9056945b0cfcf72c565f35ec2fa06a8c
Jonatan Ballestero <118026712+JonBalles@users.noreply.github.com>
2024-05-22 19:49:20 -0300

    Img corregidos, falta selector de imagen por indice, media querys en home

--- JS/banner.js
+++ JS/banner.js
@@ -3,47 +3,63 @@ const dotBox = document.querySelector(".puntitos")
 let indice = 0;
 
 function puntitos(){
-  for(i =0 ; i < 3 ; i++){
-    let puntito = document.createRange().createContextualFragment(`
-    <label class="label" for="${img[i]}">
-    <input id="${"dot"+[i]}" type="radio" name="grupoBanner">
-    <div class="button">
-    <span class="shadow"></span>
-    `)
-    dotBox.append(puntito);
-  }
-  let check = document.getElementById(`dot${indice}`)
-// Lo dejo aca pero la idea es que tome el indice actual para ubicar el punto activo y darle color
- // check.check
+  dotBox.innerHTML = '';
   
+  for(i =0 ; i < img.length ; i++){
+    
+      let puntito = document.createRange().createContextualFragment(`
+      <label class="label">
+      <input id="dot${[i]}" type="radio" name="grupoBanner">
+      <div class="button">
+      <span class="shadow"></span>
+      `);
+      dotBox.append(puntito);
+  
+  document.getElementById(`dot${i}`).addEventListener("click",() => {
+    indice = i;
+    mostrarImg()
+  })
+
+    }
 }
+
+
+
 
 function mostrarImg() {
   
+  function seleccionarDot(i){
+    let check = document.getElementById(`dot${i}`)
+    return check.checked = true
+  }
+
   for (let i = 0; i < img.length; i++) {
     img[i].style.display = "none";  
   }
-  indice++;
 
   if (indice >= img.length) {
     indice = 0;
-  } else if (indice <= 0) {
+  } else if (indice < 0) {
     indice = img.length - 1;
   }
   
   img[indice].style.display = "block";
+  seleccionarDot(indice)
 }
 
+
+
 document.getElementById("prevBtn").addEventListener("click", () => {
+  indice--;
   mostrarImg();
+  console.log(indice)
 });
 
 document.getElementById("nextBtn").addEventListener("click", () => {
   indice++;
   mostrarImg();
+  console.log(indice)
 });
-
-
 
 puntitos();
 mostrarImg();
