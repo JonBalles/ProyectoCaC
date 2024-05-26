dc37cc70fc8ac1f25ba3acc3b016c042d5c64bcd
Jonatan Ballestero <118026712+JonBalles@users.noreply.github.com>
2024-05-25 21:22:46 -0300

    terminando detalles

--- JS/formulario.js
+++ JS/formulario.js
@@ -1,10 +1,7 @@
-// Seleccionar el h1
-const h1 = document.querySelector('h1');
+//Componentes
+const form = document.getElementById('formContact');
 
-
-// Seleccionar un botón
-const boton = document.querySelector('button');
-
+// Guia de resolucion de error
 document.addEventListener("DOMContentLoaded", function() {
     const ayudaEnlace = document.querySelector('.form-help a');
 
@@ -25,3 +22,53 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+// Validador de mail
+document.addEventListener("DOMContentLoaded", function() {
+    const emailInput = document.getElementById('email');
+    
+    
+    form.addEventListener('submit', function(event) {
+        if (!validateEmail(emailInput.value)) {
+            alert('Por favor, ingresa una dirección de correo electrónico válida.');
+            event.preventDefault(); // Evitar el envío del formulario
+        }
+    });
+    
+    // Función para validar el formato de correo electrónico utilizando una expresión regular
+    function validateEmail(email) {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Expresión regular para validar formato de email
+        return regex.test(email);
+    }
+});
+
+
+
+//validador de checkbox
+document.addEventListener("DOMContentLoaded", function() {
+
+    form.addEventListener('submit', function(event) {
+       
+        const termsCheckbox = form.querySelector('#terms');
+        if (!termsCheckbox.checked) {
+            alert('Por favor, acepta los términos y condiciones.');
+            event.preventDefault(); 
+            return;
+        }
+        
+        
+        const camposObligatorios = form.querySelectorAll('[required]');
+        let faltanCompletar = false;
+
+        camposObligatorios.forEach(function(campo) {
+            if (!campo.value.trim()) { 
+                faltanCompletar = true;
+            }
+        });
+
+        
+        if (faltanCompletar) {
+            alert('Por favor, completa todos los campos obligatorios.');
+            event.preventDefault(); 
+        }
+    });
+});
